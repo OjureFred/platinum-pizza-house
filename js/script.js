98373f8fd7ef72440a14a7cb7ac4e5af6990d714
Fred Ojure <fredojure@hotmail.com>
2020-05-26 19:16:54 +0300

    Corrected case of wrong alculation based on size of pizza

--- js/script.js
+++ js/script.js
@@ -11,23 +11,22 @@ function pizzaOrder(size, crust, topping) {
  
 pizzaOrder.prototype.calculateCost = function(){
     //calculate based on size
-    if (this.size == "small") {
+    if (this.size === "small") {
         this.price = 600;
+    } else if (this.size === "medium") {
+        this.price = 800;
+    } else if (this.size === "large") {
+        this.price = 1000;
     }
-    if (this.size == "medium") {
-        this.price == 800;
-    }
-    if (this.size == "large") {
-        this.price == 1000;
-    }
+
     //calculate based on crust
     if (this.crust == "crispy") {
         this.price += 80;
     }
-    if (this.crust == "stuffed") {
+    else if (this.crust == "stuffed") {
         this.price += 100;
     }
-    if (this.crust == "gluten-free") {
+    else if (this.crust == "gluten-free") {
         this.price += 110;
     }
 };
